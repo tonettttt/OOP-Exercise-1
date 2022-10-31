85a7c8a25a1abee7780529c62eb368768ff19247
tonettttt <116172608+tonettttt@users.noreply.github.com>
2022-10-31 11:26:12 +0800

    Create Bicycle.java

---
+++ Bicycle.java
@@ -0,0 +1,18 @@
+public class Bicycle
+{
+	private String ownerName;
+	
+	public Bicycle ( ) {
+		ownerName = "Unknown";
+		}
+		public String getOwnerName ( ) {
+			return ownerName;
+			}
+			public void setOwnerName (String name) {
+				
+				ownerName = name;
+				
+				
+			}
+	
+}
